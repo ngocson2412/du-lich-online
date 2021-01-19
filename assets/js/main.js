659be698805e8ddf89133e8802f0dd4c071a866d
NgocSon2412 <sonnguyenk57@gmail.com>
2021-01-19 20:04:11 +0900

    release 01-19

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
 /*============================Menu Table===================
 - 1, init
 - 2, Navigation
-- 3, Box discover slider
+- 3, Happy Client Slider
+- 4, Box discover slider
 */
 /* ============================= 1, init  ============================= */
 
 $(document).ready(function() {
 	navigation.init();
+	date.init();
+	happyClient.init()
 	boxDiscoverSlider.init();
 });
 
@@ -40,7 +43,71 @@ const navigation = {
 
 	
 }
-/* ============================= 3, Box discover slider  ============================= */
+
+/* ============================= 3, Happy Client Slider  ============================= */
+
+const happyClient = {
+	init: function() {
+		this.happyClientCarousel()
+	},
+	happyClientCarousel: function() {
+		const optionsDevice = {
+			loop: true,
+			margin: 0,
+			autoplay: false,
+			dots: true,
+			dotsContainer: '.client-carousel-control .pagination-control',
+			nav: false,
+			responsive: {
+				0: {
+					items: 1
+				},
+			}
+		}
+		const optionsClient = {
+			loop: true,
+			margin: 10,
+			autoplay: true,
+			dots: false,
+			nav: true,
+			navText: ["<img src='./assets/images/happy-client-slider/icon-angle-left.svg' />","<img src='./assets/images/happy-client-slider/icon-angle-right.svg' />"],
+			navContainer: '.client-carousel-control .nav-control',
+			responsive: {
+				0: {
+					items: 2
+				},
+				1200: {
+					items: 3
+				}
+			}
+		}
+		const slideLength = document.querySelectorAll('.device-carousel .device-slide')
+		showPerPageCarousel(1, slideLength.length)
+
+		const owlDevice = $('.device-carousel').owlCarousel(optionsDevice)
+		const owlClient = $('.client-card-carousel').owlCarousel(optionsClient)
+
+		function showPerPageCarousel(perpage, pages) {
+			$('.carousel-info #client-carousel-current-slide').html(perpage)
+			$('.carousel-info #client-carousel-total').html(pages)
+		}
+
+		if (owlDevice && owlClient) {
+			// Event Change Carousel
+			owlClient.on('changed.owl.carousel', function (event) {
+				const currentIndex = event.item.index
+				owlDevice.trigger('to.owl.carousel', [currentIndex - 3, 200]);
+			})
+			owlDevice.on('changed.owl.carousel', function (event) {
+				const currentIndex = event.item.index
+				owlClient.trigger('to.owl.carousel', [currentIndex - 3, 200]);
+				showPerPageCarousel(currentIndex - 2, slideLength.length)
+			})
+		}
+	}
+}
+
+/* ============================= 4, Box discover slider  ============================= */
 const boxDiscoverSlider = {
 	init: function () {
 		this.boxDiscoverSlider();
@@ -65,5 +132,48 @@ const boxDiscoverSlider = {
 			autoplayTimeout:4000,
 			autoplayHoverPause:true,
 		})
+	},
+}
+
+/* ============================= 3, Datepicker  ============================= */
+const date = {
+	init:function(){
+		this.datePicker();
+	},
+	datePicker:function(){
+		//set current date
+		var today = moment();
+		$("#dateInput").val(today.format('YYYY-MM-DD'));
+
+		//setting datepicker
+		$("#datepicker").datepicker({
+			format: 'yyyy-mm-dd',
+			autoclose: true,
+			language: 'vi',
+			startDate: "currentDate",
+		});
+		today = new Date(today.format('YYYY-MM-DD'))
+		$("#datepicker").datepicker("setDate", today);
+		$("#datepicker").text(`${today.getDate()} tháng ${today.getMonth()+1}, ${today.getFullYear()}`);
+		$("#toggle").click(function(e) {
+			e.preventDefault();
+			$("#datepicker").datepicker("show");
+		})
+
+		$("#toggle_cal").click(function(e) {
+			e.preventDefault();
+			$("#datepicker").datepicker("show");
+		})
+		$("#dateLabel").click(function(e) {
+			e.preventDefault();
+			$("#datepicker").datepicker("show");
+		});
+		$("#datepicker").on("changeDate", function() {
+			var selectedDate = $("#datepicker").datepicker("getFormattedDate")
+			$("#dateInput").val(selectedDate);
+			selectedDate = new Date(selectedDate)
+			$("#datepicker").text(`${selectedDate.getDate()} tháng ${selectedDate.getMonth()+1}, ${selectedDate.getFullYear()}`);
+
+		});
 	},
 }
